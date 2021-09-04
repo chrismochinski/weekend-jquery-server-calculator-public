b1e88c01f6a8f771a688fb382789a5e5f3864057
Chris Mo <cmochinski@gmail.com>
2021-09-04 12:42:41 -0500

    basic functionality success

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,9 +10,10 @@ function readyNow() {
     $('#dividedByButton').on('click', dividedByButton);
     $('#equalsButton').on('click', submitMath);
     $('#clearButton').on('click', clearAll);
-    getTheMaths();
-    // getTheEquations();
+    // getTheMaths();
 }
+
+let operator; //assign operator variable to empty string...for now!
 
 function getTheMaths() {
     // make a request to the server for the equations array
@@ -20,49 +21,41 @@ function getTheMaths() {
 
     $.ajax({ //asynchronous sequence of events = steps, one at a time, waiting to complete
         method: 'GET',    // requesting w/ server ...calling the server, ends up in the get method /trains
-        url: '/calculate'    
-    }).then(function(response) { // "let me know when you're back, then I want you to do this with the list of equations"
+        url: '/calculate'
+    }).then(function (response) { // "let me know when you're back, then I want you to do this with the list of equations"
         // response = the listOfMaths from server
 
-        $('input').empty(); // fresh start empty! (so it doesn't post everything again)
+        // $('input').empty(); // fresh start empty! (so it doesn't post everything again)
 
         console.log('great success! Here are the equations:', response)
         const listOfMaths = response;
-        for(const math of listOfMaths) {
+        $('#listOfMathOutput').empty(); //clear list so whole array doesn't re-post in for of loop
+        for (const math of listOfMaths) {
             console.log(math);
             $('#listOfMathOutput').append(`
-                <li>${math.firstNumber}, ${math.operator}, ${math.secondNumber}</li>
+                <li>${math.firstNumber} ${math.operator} ${math.secondNumber} = ${math.product}</li>
             `)
         }
-       
     });
     console.log('After AJAX.');
 }
-function getTheMaths() {
-        $.ajax({ 
-            type: 'GET',
-            url: '/calculate'
-        }).then(response);
-} //end get equations function
-
-
-
 
 
 function clearAll() {
-if(operator === '+' || operator === '-' || operator === '*' || operator === '+' || operator === '÷') {
-    console.log($(this));
-    console.log(`Operator was: ${operator}`);
-    operator = operator[-1];
-    console.log(`Operator is now: ${operator}`);
-    $('#firstNumber').val('');
-    $('#secondNumber').val('');
-    $('#firstNumberOutput').val('');
-    $('#operatorOutput').val('');
-    $('.theCalculationOutput').text('');
-    $('button').css('background-color', '#FCFCFC')
-}
-else{}
+    if (operator === '+' || operator === '-' || operator === '*' || operator === '+' || operator === '÷') {
+        console.log($(this));
+        console.log(`Operator was: ${operator}`);
+        operator = operator[-1];
+        console.log(`Operator is now: ${operator}`);
+        $('#firstNumber').val('');
+        $('#secondNumber').val('');
+        $('#firstNumberOutput').val('');
+        $('#operatorOutput').val('');
+        $('.theCalculationOutput').text('');
+        $('button').css('background-color', '#FCFCFC')
+        // $('#listOfMathOutput').empty(); //clear list so whole array doesn't re-post in for of loop
+    }
+    else { }
 }
 
 function submitMath() { //post function to utilize inputs and button
@@ -70,49 +63,48 @@ function submitMath() { //post function to utilize inputs and button
     const mathToSend = {
         firstNumber: $('#firstNumber').val(),
         operator: operator,
-        secondNumber: $('#secondNumber').val(),
-    }
+        secondNumber: $('#secondNumber').val()
+    };
 
     //conditional for not enough info alerts...
-if(mathToSend.firstNumber == '' && mathToSend.secondNumber == ''){
-    alert(`I'm gonna need a LOT more info than that...`)
-}
-else if(mathToSend.firstNumber == '' || mathToSend.secondNumber == '') {
-    alert(`What am I supposed to do with only one number?`);
-    return;
-}
+    if (mathToSend.firstNumber == '' && mathToSend.secondNumber == '') {
+        alert(`I'm gonna need a LOT more info than that...`)
+    }
+    else if (mathToSend.firstNumber == '' || mathToSend.secondNumber == '') {
+        alert(`What am I supposed to do with only one number?`);
+        return;
+    }
+    else if (operator == undefined) {
+        alert(`What would you like me to do with these numbers?`);
+        return;
+    }
 
-else if(operator == undefined) {
-    alert(`What would you like me to do with these numbers?`);
-    return;
-}
-
-console.log(mathToSend);
+    console.log(mathToSend);
 
     $.ajax({
         method: 'POST',
         url: '/calculate',
-        data: mathToSend,
-    }).then(handlePostSuccess);
+        data: mathToSend
+    }).then(handlePostSuccess).catch(postError);
 }
 
-function handlePostSuccess(){
-    console.log('in post success!');
+function handlePostSuccess(res) {  //this is the res from the POST
+    console.log(res); // grabbing the res.send from post
+    clearAll(); //calling clear all function to clear operator and fields
+    getTheMaths();
 }
 
 function postError() { //if there's an error with the input/operator/math
-    alert('Sorry, something broke.')
+    // alert('Sorry, something broke.')
 }
 
 
-let operator; //assign operator variable to empty string...for now!
-
 
 function plusButton() {
-    console.log($(this));
-    console.log(`Operator was: ${operator}`)
+    // console.log($(this));
+    // console.log(`Operator was: ${operator}`)
     operator = '+';
-    console.log('operator is now:', operator);
+    // console.log('operator is now:', operator);
     $('#plusButton').css('background-color', '#c5edd0'); //plus button selected
     $('#minusButton').css('background-color', '#FCFCFC');
     $('#timesButton').css('background-color', '#FCFCFC');
@@ -120,10 +112,10 @@ function plusButton() {
 }
 
 function minusButton() {
-    console.log($(this));
-    console.log(`Operator was: ${operator}`)
+    // console.log($(this));
+    // console.log(`Operator was: ${operator}`)
     operator = '-';
-    console.log('operator is now:', operator);
+    // console.log('operator is now:', operator);
     $('#plusButton').css('background-color', '#FCFCFC');
     $('#minusButton').css('background-color', '#c5edd0'); //minus button selected
     $('#timesButton').css('background-color', '#FCFCFC');
@@ -131,27 +123,23 @@ function minusButton() {
 }
 
 function timesButton() {
-    console.log($(this));
-    console.log(`Operator was: ${operator}`)
+    // console.log($(this));
+    // console.log(`Operator was: ${operator}`)
     operator = '*';
-    console.log('operator is now:', operator);
-    $('#plusButton').css('background-color', '#FCFCFC'); 
+    // console.log('operator is now:', operator);
+    $('#plusButton').css('background-color', '#FCFCFC');
     $('#minusButton').css('background-color', '#FCFCFC');
     $('#timesButton').css('background-color', '#c5edd0'); //times button selected
     $('#dividedByButton').css('background-color', '#FCFCFC');
 }
 
 function dividedByButton() {
-    console.log($(this));
-    console.log(`Operator was: ${operator}`)
+    // console.log($(this));
+    // console.log(`Operator was: ${operator}`)
     operator = '÷';
-    console.log('operator is now:', operator);
+    // console.log('operator is now:', operator);
     $('#plusButton').css('background-color', '#FCFCFC');
-    $('#minusButton').css('background-color', '#FCFCFC'); 
+    $('#minusButton').css('background-color', '#FCFCFC');
     $('#timesButton').css('background-color', '#FCFCFC'); // divided by button selected
     $('#dividedByButton').css('background-color', '#c5edd0');
-}
-
-function handlePostsuccess() {
-    console.log(data);
 }
--- server/server.js
+++ server/server.js
@@ -3,34 +3,50 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+// app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('server/public'));
 
 const calculationsArray = [ //dummy array for now while working...
-    {firstNumber: 2, operator: '+', secondNumber: 2}, 
-    {firstNumber: 3, operator: '-', secondNumber: 42},
-    {firstNumber: 1, operator: '*', secondNumber: 14},
-    {firstNumber: 7, operator: '÷', secondNumber: 1},
+    // {firstNumber: 2, operator: '+', secondNumber: 2, product: 4}, 
+    // {firstNumber: 3, operator: '-', secondNumber: 42},
+    // {firstNumber: 1, operator: '*', secondNumber: 14},
+    // {firstNumber: 7, operator: '÷', secondNumber: 1},
 ];
 
 app.post('/calculate', (req, res) => {
     const equation = req.body;
     console.log(`in app.post (/calculate) grabbing equation: `);
+    crunchNumbers(equation);
     calculationsArray.push(equation);
     console.log(calculationsArray);
-    res.send(calculationsArray);
+    res.send(`We got your equation!`);  // this is NOT NECESSARY - just seding love note back in handlePOstSuccess on client side
 })
+
+function crunchNumbers(equation){ //doing math...
+    if(equation.operator === '+'){
+        equation.product = parseInt(equation.firstNumber) + parseInt(equation.secondNumber);
+    }
+    else if(equation.operator === '-'){
+        equation.product = parseInt(equation.firstNumber) - parseInt(equation.secondNumber);
+    }
+    else if(equation.operator === '*'){
+        equation.product = parseInt(equation.firstNumber) * parseInt(equation.secondNumber);
+    }
+    else if(equation.operator === '÷'){
+        equation.product = (parseInt(equation.firstNumber) / parseInt(equation.secondNumber)).toFixed(4);
+    };
+};
 
 
 app.get('/calculate', (req, res) => {
     console.log('As requested, here come the equations...');
-    const thisEquation = req.body;
-    calculationsArray.push(thisEquation);
-    console.log(thisEquation);
-    res.send(calculationsArray);
+    // const equationToAdd = req.body;
+    // calculationsArray.push(equationToAdd);
+    // console.log(equationToAdd);
+    res.send(calculationsArray); //send the array to the client to do stuff with it (in thi case - see getTheMaths - function(response))
 });
-
-
-
 
 
 
